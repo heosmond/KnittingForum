4904ff4df0f3f9d5c76896beb9241fe8f0ae35b9
heosmond <osmondhe@gmail.com>
2025-03-07 17:36:31 -0400

    ran migration

--- KnittingForum/Migrations/20250307213356_CustomUserData.Designer.cs
+++ KnittingForum/Migrations/20250307213356_CustomUserData.Designer.cs
@@ -4,6 +4,7 @@ using KnittingForum.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KnittingForum.Migrations
 {
     [DbContext(typeof(KnittingForumContext))]
-    partial class KnittingForumContextModelSnapshot : ModelSnapshot
+    [Migration("20250307213356_CustomUserData")]
+    partial class CustomUserData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
